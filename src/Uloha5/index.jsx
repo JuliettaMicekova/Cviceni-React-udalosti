ae873df62dc16e064a38f447e54ba0323335c924
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-05-12 19:25:29 +0200

    uloha 4 a 5

--- src/Uloha5/index.jsx
+++ src/Uloha5/index.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 // Zadání 1: Dopiš komponentu `NakupniPolozka`, aby v divu byl vypsaný název položky a tlačítko s textem „koupit“.
 // Zadání 2: Po kliknutí na tlačítko vypiš do konzole „Uživatel chce koupit DOPLŇ_NÁZEV_POLOŽKY“.
 
 export const NakupniPolozka = ({ nazev }) => {
-	return <div>@TODO</div>
-}
+  return (
+    <div>
+      {nazev}
+      <button
+        onClick={() => {
+          console.log(`Uzivatel chce koupit ${nazev}`);
+        }}
+      >
+        koupit
+      </button>
+    </div>
+  );
+};
 
 // Zadání 3: Pomocí `polozky.map` vypiš všechny názvy jako nákupní položky.
 
 export const Uloha5 = () => {
-	const polozky = ['hrušky', 'jablka', 'třešně', 'jahody']
+  const polozky = ['hrušky', 'jablka', 'třešně', 'jahody'];
 
-	return (
-		<>
-			<NakupniPolozka nazev="hrušky" />
-			<NakupniPolozka nazev="jablka" />
-		</>
-	)
-}
+  return (
+    <>
+      {polozky.map((polozka) => (
+        <NakupniPolozka nazev={polozka} />
+      ))}
+    </>
+  );
+};
