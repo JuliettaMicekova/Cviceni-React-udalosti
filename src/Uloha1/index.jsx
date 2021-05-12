77ccb23835b61262c1a17214caca37370540ed4e
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-05-12 18:34:00 +0200

    uloha 1 a 2

--- src/Uloha1/index.jsx
+++ src/Uloha1/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 // Zadání: Po kliku na tlačítko zobraz bublinu s textem „Ahoj“.
 
 export const Uloha1 = () => {
-	return <button>Ukaž bublinu</button>
-}
+  return (
+    <button className="button" onClick={clickOn}>
+      Ukaž bublinu
+    </button>
+  );
+};
+
+const clickOn = () => {
+  alert('Ahoj!');
+};
